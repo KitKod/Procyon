0b4608b1ee549cc966d03862d9ee7828ccba8d90
Anton Trukhan <deko71dekobraz@gmail.com>
2021-12-03 21:59:28 +0200

    feat: add wrapper to global view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsModule } from '@ngxs/store';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
         BrowserAnimationsModule,
         NgxsModule.forRoot(),
         NgxsLoggerPluginModule.forRoot(),
+        MatSidenavModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
