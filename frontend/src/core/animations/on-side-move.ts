c0cec695cb9e8befc0aeefa291884be4fdc104cb
Anton Trukhan <deko71dekobraz@gmail.com>
2021-12-17 17:53:39 +0200

    fix: correct lint issues

--- frontend/src/core/animations/on-side-move.ts
+++ frontend/src/core/animations/on-side-move.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 export const onTextAppear = trigger('onTextAppear', [
     transition('open => void', [animate('250ms ease-in'), style({ width: 0 })]),
